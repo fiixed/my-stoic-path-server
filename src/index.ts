0622bf0ab544c2210247ca581e93bb88e8a8d646
Andrew Bell <fiixed@gmail.com>
2023-08-30 13:41:50 -0500

    get timestamp and journal entry from postman

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 const PORT = process.env.port || 8000;
 
-app.get('/', (req, res) => {
-    res.send('<h1>test</h1>')
+app.post('/', (req, res) => {
+  console.log(req.body);
+
+  res.json({ message: 'I am listening' });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on ${PORT}`);
+  console.log(`Server is listening on ${PORT}`);
 });
